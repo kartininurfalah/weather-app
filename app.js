6cbfd10239f9b7af2566654c86b1f0992cc5d08d
kartininurfalah <kartininurfalah@gmail.com>
2018-04-15 19:50:01 +0700

    update

--- app.js
+++ app.js
@@ -4,5 +4,8 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=AIzaSyALfWvVw3rZspKgRhd11AH0JFU3FD8Lt0Q',
   json: true
 }, (error, response, body) => {
-  console.log(body);
+  //console.log(JSON.stringify(response, undefined, 2));
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude : ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
