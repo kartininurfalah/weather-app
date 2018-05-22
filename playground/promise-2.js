75c6583f0a48679a878cce7fe1b7e36c726f80b5
Kartini Nurfalah <kartininurfalah@gmail.com>
2018-05-22 16:02:47 +0700

    add promise

---
+++ playground/promise-2.js
@@ -0,0 +1,9 @@
+var geocodeAddress = (address) => {
+
+};
+
+geocodeAddress('19146').then((location) => {
+    console.log(JSON.stringify(location, undefined, 2));
+}, (errorMessage) => {
+    console.log(errorMessage);
+});
